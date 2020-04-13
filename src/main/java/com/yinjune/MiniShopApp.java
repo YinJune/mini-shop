e1cb6429569552ba155b8559d6ddcc9ae690c9d9
殷俊杰 <247385709@qq.com>
2020-04-13 23:32:25 +0800

    在凄凉的大地上写下：相信未来

---
+++ src/main/java/com/yinjune/MiniShopApp.java
@@ -0,0 +1,16 @@
+package com.yinjune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: YinJunJie
+ * @date: 2020/4/13 23:28
+ * @description:
+ */
+@SpringBootApplication
+public class MiniShopApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MiniShopApp.class, args);
+    }
+}
